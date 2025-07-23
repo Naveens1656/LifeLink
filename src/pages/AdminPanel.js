bd5d0ddd68549af594eba7e9b397bda8f9b0b7c3
gkishor00 <kishorg2005gk@gmail.com>
2025-07-23 18:01:18 +0530

    AdminPanel.js

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function AdminPanel() {
+return (
+<div className="container mt-4">
+<h3>Admin Panel</h3>
+<p>Manage users and requests.</p>
+</div>
+);
+}
+
+export default AdminPanel;
