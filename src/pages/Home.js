0aafc11a23d3098a985fb32b1fecdc6319de01af
Naveens1656 <sn770324@gmail.com>
2025-07-23 14:43:39 +0530

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,18 @@
+import React, { useState, useEffect } from "react";
+import API from "../api";
+import DonorCard from "../components/DonorCard";
 
+function Home() {
+const [donors, setDonors] = useState([]);
+
+useEffect(() => {
+API.get("/donors").then(res => setDonors(res.data));
+}, []);
+
+return (
+<div className="container mt-4">
+<h3>Available Donors</h3>
+{donors.map(d => <DonorCard key={d._id} donor={d} />)}
+</div>
+);
+}
