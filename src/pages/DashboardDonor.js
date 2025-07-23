cc91554ff610931b1a27c74be0633893a7718ed4
gkishor00 <kishorg2005gk@gmail.com>
2025-07-23 17:59:57 +0530

    DashboardDonor.js

--- src/pages/DashboardDonor.js
+++ src/pages/DashboardDonor.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function DashboardDonor() {
+return (
+<div className="container mt-4">
+<h3>Welcome Donor</h3>
+<p>You can update your availability and donation history here.</p>
+</div>
+);
+}
+
+export default DashboardDonor;
