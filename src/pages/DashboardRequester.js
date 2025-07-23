d7884ece6cada6ddd6e869001621d753edb6227c
gkishor00 <kishorg2005gk@gmail.com>
2025-07-23 17:58:16 +0530

    DashboardRequester.js

--- src/pages/DashboardRequester.js
+++ src/pages/DashboardRequester.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function DashboardRequester() {
+return (
+<div className="container mt-4">
+<h3>Welcome Requester</h3>
+<p>Submit a blood request or check donor matches.</p>
+</div>
+);
+}
+
+export default DashboardRequester;
